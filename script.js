0513215d32017b52a0185634444fa9d140f2a2e2
Jop <jopbrandenburg@gmail.com>
2022-11-26 18:51:50 +0100

    added profile page

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//TODO: js uitsplitsen over verschillende files voor de verschillende html files
+// vervolgens hobby meegeven over de verschillende files heen? Kan dat? 
+
 
 disliked = 0;
 
@@ -32,6 +35,8 @@ function like(){
 		alert("huh?")
 	}
 }
+
+
 messageCounter=0
 hapjeEtenSent=false;
 gesprekAfgerond=false;
@@ -46,7 +51,7 @@ function sendMessage(){
 		setTimeout(function(){
 			message.append("<p class='from-them'>Haha, dat is een hele goeie!</p>")
 			setTimeout(function(){
-				message.append("<p class='from-them'>Zin om misschien eens samen een hapje te eten? Je cadeautje zou daar wel eens bij kunnen helpen :)</p>")
+				message.append("<p class='from-them'>Zin om misschien eens samen een hapje te eten? Je cadeautje zou daar wel eens bij kunnen helpen :) Ik kan je misschien wel helpen met het vinden van je cadeau!</p>")
 				hapjeEtenSent=true;
 			},5000)
 		},2000)
@@ -68,115 +73,6 @@ function sendMessage(){
 	messageCounter++;
 }	
 
- document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems);
-    var elem = $("#opstartModal");
-    M.Modal.getInstance(elem).open();
-
-  });
-
-function cardClicked(card){
-	if(cards[card].isFlipped){
-		flip(card);
-		return;
-	}
-
-	if(numberFlipped() >1) {
-		flipOpenCardsBackClosed();
-	}
-
-	flipCounter++;
-	$('#flipCounter').html(flipCounter)
-	flip(card);
-	
-	setTimeout(function(){
-		if(cards[card].isFlipped){
-			flip(card)	
-		}
-	}, 3000)
-}
-
-function flip(card) {
-	if(cards[card].completed){
-		return;
-	}
-
-	var frontDeg, backDeg;
-	var isFlipped = cards[card].isFlipped
-	var frontside = $("#"+card +" .cardFrontside");
-	var backside = $("#"+card+" .cardBackside");
-
-	if(isFlipped){
-		frontDeg = 0
-		backDeg = 180
-	}else{
-		frontDeg = 180
-		backDeg = 0
-	}
-
-	frontside.css("transition-duration", "1s")
-	frontside.css("transform", "rotatey(" + frontDeg + "deg)")
-	backside.css("transitionDuration", "1s")
-	backside.css("transform", "rotatey(" + backDeg + "deg)")
-	cards[card].isFlipped = !isFlipped
-	cardIsCompleted(cards[card]);
-}
-
-function numberFlipped(){
-	var numberFlipped=0
-	for (var key in cards){
-		var card = cards[key]
-		if(card.isFlipped && !card.completed){
-			numberFlipped++;
-		}
-	}
-	return numberFlipped;
-}
-
-function flipOpenCardsBackClosed(){
-	for (var key in cards){
-		var card = cards[key]
-		if(card.isFlipped){
-			flip(card.id)
-		}
-	}
-}
-
-function cardIsCompleted(card){
-	if(card.completed){
-		return true;
-	}
-	buddyCard = cards[card.buddy]
-	if(card.isFlipped && buddyCard.isFlipped){
-		console.log(card.id + " en " + buddyCard.id + " IS COMPLETED!!!!!!!!")
-		card.completed = true;
-		buddyCard.completed = true;
-		$("#"+card.id).css("background-color", card.completedColor);
-		$("#"+buddyCard.id).css("background-color", buddyCard.completedColor);
-		isGameDone();
-		return true;
-	}else{
-		return false;
-	}
-}
-
-function isGameDone(){
-	for(var key in cards){
-		var card = cards[key]
-		if(!card.completed){
-			console.log(card)
-			return false;
-		}
-	}
-	if(flipCounter > 30){
-		var elem = $("#afsluitModalNietGehaald");
-    	M.Modal.getInstance(elem).open();	
-    	$('#finalFlipCounterNietGehaald').html(flipCounter)
-    	return;
-	}else{
-		var elem = $("#afsluitModal");
-	    M.Modal.getInstance(elem).open();
-	    $('#finalFlipCounter').html(flipCounter)
-	}
+function saveProfile(){
+	window.location.assign('index.html');
 }
