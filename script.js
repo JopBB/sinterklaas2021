9bb4d3a66973d21946d4207d69dc4ade3b648af8
Jop <jopbrandenburg@gmail.com>
2022-11-27 11:39:05 +0100

    chat verbeteringen

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function like(){
 			window.location.assign('messages.html')
 		}, 2000)
 	}else{
-		alert("huh?")
+		alert("Weet je dit zeker? Misschien komt je droomman nog wel later voorbij")
 	}
 }
 
@@ -40,6 +40,7 @@ function like(){
 messageCounter=0
 hapjeEtenSent=false;
 gesprekAfgerond=false;
+raadselSent=false;
 function sendMessage(){
 	var input = $("#userInput");
 	var message = $(".imessage");
@@ -47,30 +48,62 @@ function sendMessage(){
 
 
 	message.append(newMessage);
+	$(".imessage").scrollTop($(".imessage").height()+1000);
+	if(raadselSent){
+		console.log('lalalala')
+		if(input.val().includes("1") || input.val().includes("een")){
+			setTimeout(function(){
+				message.append("<p class='from-them'>Helemaal goed! Je cadeautje moet je zoeken in de gangkast! Zie je snel!</p>")
+				$(".imessage").scrollTop($(".imessage").height()+1000);
+				gesprekAfgerond=true;
+			},3000)
+			gesprekAfgerond=true;
+		}else{
+			setTimeout(function(){
+				message.append("<p class='from-them'>Nee, dat klopt niet helemaal! Probeer het nog eens?</p>")
+				$(".imessage").scrollTop($(".imessage").height()+1000);
+			},3000)
+		}
+	}
 	if(messageCounter<1){
 		setTimeout(function(){
 			message.append("<p class='from-them'>Haha, dat is een hele goeie!</p>")
+			$(".imessage").scrollTop($(".imessage").height()+1000);
 			setTimeout(function(){
-				message.append("<p class='from-them'>Zin om misschien eens samen een hapje te eten? Je cadeautje zou daar wel eens bij kunnen helpen :) Ik kan je misschien wel helpen met het vinden van je cadeau!</p>")
+				message.append("<p class='from-them'>Heb je misschien zin om eens samen een hapje te eten? Je cadeautje zou daar wel eens bij kunnen helpen :) Dan help ik je met het vinden van je cadeau!</p>")
 				hapjeEtenSent=true;
+				$(".imessage").scrollTop($(".imessage").height()+1000);
 			},5000)
 		},2000)
-	}else if(hapjeEtenSent && !gesprekAfgerond){
+	}else if(hapjeEtenSent && !gesprekAfgerond && !raadselSent){
+
 		if(input.val().includes("ja") || input.val().includes("leuk")){
 			setTimeout(function(){
-				message.append("<p class='from-them'>Oke! Gezellig. Je cadeautje kan je vinden waar de wijntjes liggen!</p>")
+				message.append("<p class='from-them'>Oke! Gezellig. Dan zal ik je nu helpen met het vinden van je cadeau. Als je dit raadsel oplost zal ik je helpen:</p>")
+				$(".imessage").scrollTop($(".imessage").height()+1000);
 				setTimeout(function(){
-					message.append("<p class='from-them'>Zie je snel!</p>")
-					gesprekAfgerond=true;
-				},2000)
-			},2000)
+					message.append("<p class='from-them'>Onderweg naar de winkel liep ik een vrouw tegemoet. Deze vrouw had 2 rugzakken met in elke rugzak 3 katten, met elk 3 kittens. Katten en mensen bij elkaar, met z’n hoe velen liepen we naar de winkel?</p>")
+					raadselSent=true;
+					$(".imessage").scrollTop($(".imessage").height()+1000);
+				},3000)
+			},5000)
 		}else{
-			message.append("<p class='from-them'>Oh jammer. Nou, dan heb je vast ook niks aan je cadeautje, laat dan maar.</p>")
-			gesprekAfgerond=true;
+			setTimeout(function(){
+				message.append("<p class='from-them'>Oh jammer. Nou, dan heb je vast ook niks aan je cadeautje, laat dan maar.</p>")
+				gesprekAfgerond=true;
+				$(".imessage").scrollTop($(".imessage").height()+1000);
+			},2000)
 		}
+	}else if(gesprekAfgerond && input.val().includes("sorry")){
+		setTimeout(function(){
+				message.append("<p class='from-them'>Nou oke dan.. Toch zin om een keer met me te eten?")
+				$(".imessage").scrollTop($(".imessage").height()+1000);
+				gesprekAfgerond=false;
+			},5000)
 	}
 	input.val('')
 	messageCounter++;
+	$(".imessage").scrollTop($(".imessage").height()+1000);
 }	
 
 function saveProfile(){
