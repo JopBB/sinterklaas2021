851bb7dec2599247dd1e7d3ac0d0f5203d2ad5db
Jop <jopbrandenburg@gmail.com>
2022-11-30 13:41:50 +0100

    bugfixes

--- script.js
+++ script.js
@@ -66,13 +66,14 @@ function sendMessage(){
 	}
 
 	if(raadsel1 && !raadsel2 && !laatstehartje){
-		if(input.val().toLowerCase().includes("Schiphol")){
+		if(input.val().toLowerCase().includes("schiphol")){
 			setTimeout(function(){
 				message.append("<p class='from-them'>Ooh ja! Goed gevonden, dankjewel!</p>")
 				$(".imessage").scrollTop($(".imessage").height()+1000);
 				hartjelevel.html("2/4");
 				hartjeicon.css("color","pink");
 				setTimeout(function(){
+					alert("Je hebt Jouke goed geholpen, hij vindt je weer wat leuker!")
 					message.append("<p class='from-them'>Ik ben bijna bij het schip aangekomen, maar ik loop nu tegen nog een probleempje aan... Ik weet niet zeker of ik het schip wel op kom. Aan het schip hangt een laddertje van drie meter lengte met om de 20 cm een sport. Als het eb is, steken er acht sporten uit het water. Bij vloed stijgt het water met 1.65 meter. Het is nu vloed, en ik weet niet zeker of ik wel op de boot kom. Kan jij anders alvast even uitrekenen: hoeveel sporten er tijdens vloed nog boven het water uitkomen?</p>")
 					$(".imessage").scrollTop($(".imessage").height()+1000);
 					raadsel2=true;
@@ -80,14 +81,14 @@ function sendMessage(){
 			},3000)
 		}else{
 			setTimeout(function(){
-				message.append("<p class='from-them'>Nee, het lijkt me niet dat ik via \" "+inputval+" \" bij het schip kan komen! Probeer het nog eens?</p>")
+				message.append("<p class='from-them'>Nee, het lijkt me niet dat ik via \""+inputval+"\" bij het schip kan komen! Probeer het nog eens?</p>")
 				$(".imessage").scrollTop($(".imessage").height()+1000);
 			},3000)
 		}
 	}else if(raadsel1 && raadsel2 && !laatstehartje){
 		if(input.val().includes("8") || input.val().toLowerCase().includes("acht")){
 			setTimeout(function(){
-				message.append("<p class='from-them'>Ooh tuurlijk! Ik kan het schip op! Dankjewel!</p>")
+				message.append("<p class='from-them'>Ooh... tuurlijk! Ik kan het schip op! Je bent de beste! Dankjewel!</p>")
 				$(".imessage").scrollTop($(".imessage").height()+1000);
 				hartjelevel.html("3/4")
 				hartjeicon.css("color","#ff5f79");
@@ -105,8 +106,7 @@ function sendMessage(){
 			},3000)
 		}
 	}
-
-	if(messageCounter<1){
+	if(messageCounter<1 && input.val().split(" ").length > 5){
 		setTimeout(function(){
 			message.append("<p class='from-them'>Haha, zelden zo'n goede openingszin gehoord!</p>")
 			hartjelevel.html("1/4")
@@ -127,12 +127,13 @@ function sendMessage(){
 				message.append("<p class='from-them'>Ik moet een schip blussen, maar ik weet niet hoe ik er moet komen. Weet jij hoe ik er kan komen? Ik heb deze hint gekregen:</p>")
 				message.append("<img src='./images/boot.jpg'>")
 				raadsel1=true;
-				$(".imessage").scrollTop($(".imessage").height()+1000);
+				setTimeout(function(){
+					$(".imessage").scrollTop($(".imessage").height()+1000);
+				},200)
 			},5000)
 		},2000)
 	}
 	input.val('')
-	messageCounter++;
 	$(".imessage").scrollTop($(".imessage").height()+1000);
 }	
 
