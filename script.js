6ed79a555a44897fdc4aa6fff8e9e1aef7e7b22f
Jop <jopbrandenburg@gmail.com>
2022-11-30 13:18:01 +0100

    conversation adjustments

--- script.js
+++ script.js
@@ -41,21 +41,37 @@ messageCounter=0
 gesprekAfgerond=false;
 raadsel1=false;
 raadsel2=false;
+laatstehartje=false;
 
 function sendMessage(){
 	var input = $("#userInput");
 	var message = $(".imessage");
 	var newMessage = "<p class='from-me'>" + input.val() + "</p>"
 	var inputval = input.val();
+	var hartjelevel = $("#hartjelevel");
+	var hartjeicon = $("#hartjeicon")
 
 	message.append(newMessage);
 	$(".imessage").scrollTop($(".imessage").height()+1000);
 
-	if(raadsel1 && !raadsel2){
+	if(laatstehartje){
+		if(input.val().includes("leuk") || input.val().includes("knap") || input.val().includes("mooi") || input.val().includes("grappig") || input.val().includes("fijn")){
+			hartjelevel.html("4/4");
+			hartjeicon.css("color","red");
+			setTimeout(function(){
+				message.append("<p class='from-them'>Ooh, ik moet er helemaal van blozen... Ik zal je helpen met het vinden van je cadeau! Zoek in de wijnkast, daar kan je hemt vast vinden!</p>")
+				$(".imessage").scrollTop($(".imessage").height()+1000);
+			},3000)
+		}
+	}
+
+	if(raadsel1 && !raadsel2 && !laatstehartje){
 		if(input.val().includes("schiphol")){
 			setTimeout(function(){
 				message.append("<p class='from-them'>Ooh ja! Goed gevonden, dankjewel!</p>")
 				$(".imessage").scrollTop($(".imessage").height()+1000);
+				hartjelevel.html("2/4");
+				hartjeicon.css("color","pink");
 				setTimeout(function(){
 					message.append("<p class='from-them'>Ik ben bijna bij het schip aangekomen, maar ik loop nu tegen nog een probleempje aan... Ik weet niet zeker of ik het schip wel op kom. Aan het schip hangt een laddertje van drie meter lengte met om de 20 cm een sport. Als het eb is, steken er acht sporten uit het water. Bij vloed stijgt het water met 1.65 meter. Het is nu vloed, en ik weet niet zeker of ik wel op de boot kom. Kan jij anders alvast even uitrekenen: hoeveel sporten er tijdens vloed nog boven het water uitkomen?</p>")
 					$(".imessage").scrollTop($(".imessage").height()+1000);
@@ -68,14 +84,18 @@ function sendMessage(){
 				$(".imessage").scrollTop($(".imessage").height()+1000);
 			},3000)
 		}
-	}else if(raadsel1 && raadsel2){
+	}else if(raadsel1 && raadsel2 && !laatstehartje){
 		if(input.val().includes("8") || input.val().includes("acht")){
 			setTimeout(function(){
 				message.append("<p class='from-them'>Ooh tuurlijk! Ik kan het schip op! Dankjewel!</p>")
 				$(".imessage").scrollTop($(".imessage").height()+1000);
+				hartjelevel.html("3/4")
+				hartjeicon.css("color","#ff5f79");
 				setTimeout(function(){
-					message.append("<p class='from-them'>Het schip is geblust dankzij jouw hulp. Nu zal ik je helpen met het vinden van je cadeau, maar alleen als je belooft die eens te gebruiken tijdens een date met mij! Je kan hem vinden waar de wijn opgeslagen ligt.</p>");
+					message.append("<p class='from-them'>Het schip is geblust dankzij jouw hulp!</p>");
 					$(".imessage").scrollTop($(".imessage").height()+1000);
+					alert("Jouke vindt je behoorlijk leuk inmiddels... Hoe zou je het laatste stukje van z'n hart kunnen veroveren?");
+					laatstehartje = true;
 				},2000)
 			},3000)
 		}else{
@@ -89,12 +109,15 @@ function sendMessage(){
 	if(messageCounter<1){
 		setTimeout(function(){
 			message.append("<p class='from-them'>Haha, zelden zo'n goede openingszin gehoord!</p>")
+			hartjelevel.html("1/4")
+			hartjeicon.css("color","#fbe2e6");
 			$(".imessage").scrollTop($(".imessage").height()+1000);
 			setTimeout(function(){
+				alert("Jouke vond je opmerking grappig, en hij vindt je daardoor een beetje leuker. Verover Jouke's hart (rechstbovenin) om je cadeautje te vinden!")
 				message.append("<p class='from-them'>Kan ik je ergens mee helpen?</p>")
 				$(".imessage").scrollTop($(".imessage").height()+1000);
 				messageCounter++;
-			},5000)
+			},3500)
 		},2000)
 	}else if(!raadsel1 && !raadsel2 && input.val().includes('cadeau')){
 		setTimeout(function(){
